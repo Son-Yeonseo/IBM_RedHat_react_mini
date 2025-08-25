b1ea346bc72700c524dc8eb3317414ad26c53a54
plzswitchoff <ara3ng@gmail.com>
2025-08-25 18:16:56 +0900

    localStorage key currUser로 변경

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -15,7 +15,7 @@ const Login = () => {
 
     if (currUser) {
       alert(`환영합니다, ${currUser.userNickname}님!`);
-      localStorage.setItem("loginUser", JSON.stringify(currUser));
+      localStorage.setItem("currUser", JSON.stringify(currUser));
       navigate("/Home");
     } else {
       alert("아이디 또는 비밀번호가 틀립니다.");
